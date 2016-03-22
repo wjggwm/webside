6f6104e9de16c7295b8fd98052ca9ed4995c7ee6
wjggwm@126.com <wjggwm@126.com>
2016-03-22 16:58:07 +0800

    脚本更新

--- webside-dtgrid/src/main/resources/webside.sql
+++ webside-dtgrid/src/main/resources/webside.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2016-02-29 11:22:25
+Date: 2016-03-22 16:56:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,7 +26,36 @@ CREATE TABLE `tb_login_info` (
   `l_ip` varchar(255) DEFAULT NULL,
   `l_login_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`l_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=412 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=444 DEFAULT CHARSET=utf8;
+
+
+-- ----------------------------
+-- Table structure for tb_log_info
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_log_info`;
+CREATE TABLE `tb_log_info` (
+  `l_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `u_id` bigint(20) NOT NULL,
+  `l_account_name` varchar(100) DEFAULT NULL,
+  `l_operation` varchar(255) DEFAULT NULL COMMENT '用户所做的操作',
+  `l_content` varchar(1000) DEFAULT NULL COMMENT '日志内容',
+  `l_create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建日期',
+  PRIMARY KEY (`l_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tb_log_info
+-- ----------------------------
+INSERT INTO `tb_log_info` VALUES ('1', '4', null, 'update', 'update[参数1，类型：UserEntity，值：(getUserInfo : UserInfo [id=10, sex=null, birthday=null, telephone=null, email=null, address=null, createTime=null])(getDescription : 普通用户)(getRole : RoleEntity [id=3, name=null, key=null, status=null, description=null, createTime=null, updateTime=null, userList=null])(getUserName : 普通用户)(getCreatorName : wjggwm)(getAccountName : 381543724@qq.com)]', '2016-03-07 23:50:36');
+INSERT INTO `tb_log_info` VALUES ('2', '4', 'wjggwm@126.com', 'update', 'update[参数1，类型：UserEntity，值：(getUserInfo : UserInfo [id=10, sex=null, birthday=null, telephone=null, email=null, address=null, createTime=null])(getDescription : 普通用户1)(getRole : RoleEntity [id=3, name=null, key=null, status=null, description=null, createTime=null, updateTime=null, userList=null])(getUserName : 普通用户)(getAccountName : 381543724@qq.com)(getCreatorName : wjggwm)]', '2016-03-08 22:10:51');
+INSERT INTO `tb_log_info` VALUES ('3', '4', 'wjggwm@126.com', 'update', 'update[参数1，类型：UserEntity，值：(getUserInfo : UserInfo [id=10, sex=null, birthday=null, telephone=null, email=null, address=null, createTime=null])(getDescription : 普通用户)(getRole : RoleEntity [id=3, name=null, key=null, status=null, description=null, createTime=null, updateTime=null, userList=null])(getUserName : 普通用户)(getAccountName : 381543724@qq.com)(getCreatorName : wjggwm)]', '2016-03-08 22:16:52');
+INSERT INTO `tb_log_info` VALUES ('4', '4', 'wjggwm@126.com', 'update', 'update[参数1，类型：RoleEntity，值：(getName : 普通用户)(getKey : customer)(getDescription : 普通用户1)]', '2016-03-08 22:47:51');
+INSERT INTO `tb_log_info` VALUES ('5', '4', 'wjggwm@126.com', 'insert', 'permissionUI[参数1，类型：BindingAwareModelMap，值：][参数2，类型：ShiroHttpServletRequest，值：(getRemoteUser : wjggwm@126.com)(getSession : org.apache.shiro.web.servlet.ShiroHttpSession@2de26cc8)(getRequestedSessionId : 914dd057-e5cf-415a-a760-349ea2a00c0a)(getUserPrincipal : wjggwm@126.com)][参数3，类型：Long，值：]', '2016-03-08 22:48:36');
+INSERT INTO `tb_log_info` VALUES ('6', '4', 'wjggwm@126.com', 'insert', 'permission[参数1，类型：Integer，值：][参数2，类型：String，值：(getBytes : [B@7852917d)]', '2016-03-08 22:48:46');
+INSERT INTO `tb_log_info` VALUES ('7', '4', 'wjggwm@126.com', 'update', 'update[参数1，类型：RoleEntity，值：(getName : 普通用户)(getKey : customer)(getDescription : 普通用户)]', '2016-03-08 22:55:36');
+INSERT INTO `tb_log_info` VALUES ('8', '4', 'wjggwm@126.com', 'permission', 'permission[参数1，类型：Integer，值：][参数2，类型：String，值：(getBytes : [B@4d0dcd1a)]', '2016-03-08 22:55:56');
+INSERT INTO `tb_log_info` VALUES ('9', '4', 'wjggwm@126.com', 'update', 'update[参数1，类型：ResourceEntity，值：(getName : Druid监控)(getType : 0)(getDescription : Druid监控)(getChildren : [])(getIcon : )(getSourceKey : monitor:druid)(getSourceUrl : /druid)(getParentId : 6)(getIsHide : 0)(getIsExpanded : false)(getIsLeaf : false)(getLoaded : true)(getSelected : false)]', '2016-03-20 15:00:53');
+INSERT INTO `tb_log_info` VALUES ('10', '4', 'wjggwm@126.com', 'update', 'update[参数1，类型：ResourceEntity，值：(getName : Sirona监控)(getType : 0)(getDescription : Sirona监控)(getChildren : [])(getIcon : )(getParentId : 6)(getIsHide : 0)(getIsExpanded : false)(getIsLeaf : false)(getLoaded : true)(getSelected : false)(getSourceKey : monitor:sirona)(getSourceUrl : /sirona)]', '2016-03-20 15:08:24');
 
 -- ----------------------------
 -- Table structure for tb_resource
@@ -46,7 +75,7 @@ CREATE TABLE `tb_resource` (
   `s_create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `s_update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`s_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COMMENT='资源表';
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COMMENT='资源表';
 
 -- ----------------------------
 -- Records of tb_resource
@@ -57,10 +86,10 @@ INSERT INTO `tb_resource` VALUES ('3', '2', '用户管理', 'system:user', '0', 
 INSERT INTO `tb_resource` VALUES ('4', '2', '角色管理', 'system:role', '0', '/role/listUI.html', '2', '', '0', '角色管理', '2016-01-11 22:51:07', '2016-01-22 11:07:04');
 INSERT INTO `tb_resource` VALUES ('5', '2', '资源管理', 'system:resource', '0', '/resource/listUI.html', '2', '', '0', '资源管理', '2016-01-11 22:51:55', '2016-01-22 11:07:10');
 INSERT INTO `tb_resource` VALUES ('6', null, '系统监控管理', 'monitor', '0', '', '1', 'fa fa-pencil-square-o', '0', '系统监控管理', '2016-01-05 12:11:12', '2016-02-25 14:07:48');
-INSERT INTO `tb_resource` VALUES ('7', '6', '实时监控', 'monitor:realtime', '0', '/role/listUI.html', '2', '', '0', '实时监控', '2016-01-05 12:11:12', '2016-01-22 11:05:50');
-INSERT INTO `tb_resource` VALUES ('8', '6', '告警列表', 'monitor:warning', '0', '/role/listUI.html', '2', '', '0', '告警列表', '2016-01-11 22:45:27', '2016-01-22 11:05:55');
-INSERT INTO `tb_resource` VALUES ('9', null, '登录信息管理', 'logininfo', '0', '', '1', 'fa fa-tag', '0', '登录信息管理', '2016-01-11 22:46:39', '2016-02-25 14:07:51');
-INSERT INTO `tb_resource` VALUES ('10', '9', '用户登录信息', 'logininfo:user', '0', '/loginInfo/listUI.html', '2', '', '0', '用户登录信息', '2016-01-11 22:47:41', '2016-01-22 11:06:10');
+INSERT INTO `tb_resource` VALUES ('7', '6', 'Sirona监控', 'monitor:sirona', '0', '/sirona', '2', '', '0', 'Sirona监控', '2016-01-05 12:11:12', '2016-03-20 15:08:24');
+INSERT INTO `tb_resource` VALUES ('8', '6', 'Druid监控', 'monitor:druid', '0', '/druid', '2', '', '0', 'Druid监控', '2016-01-11 22:45:27', '2016-03-20 15:00:53');
+INSERT INTO `tb_resource` VALUES ('9', null, '日志信息管理', 'logininfo', '0', '', '1', 'fa fa-tag', '0', '日志信息管理', '2016-01-11 22:46:39', '2016-03-08 21:58:17');
+INSERT INTO `tb_resource` VALUES ('10', '9', '用户登录信息', 'logininfo:userLogin', '0', '/loginInfo/listUI.html', '2', '', '0', '用户登录信息', '2016-01-11 22:47:41', '2016-03-08 22:01:52');
 INSERT INTO `tb_resource` VALUES ('11', '3', '添加', 'user:add', '1', '/user/add.html', '3', '', '0', '添加用户', '2016-01-22 00:18:40', '2016-01-22 11:06:13');
 INSERT INTO `tb_resource` VALUES ('12', '3', '编辑', 'user:edit', '1', '/user/edit.html', '3', '', '0', '编辑用户', '2016-01-22 00:18:40', '2016-01-22 11:06:22');
 INSERT INTO `tb_resource` VALUES ('13', '3', '删除', 'user:deleteBatch', '1', '/user/deleteBatch.html', null, null, '0', '删除用户', '2016-02-05 15:26:32', '2016-02-25 15:27:51');
@@ -72,6 +101,7 @@ INSERT INTO `tb_resource` VALUES ('18', '4', '分配权限', 'role:permission', 
 INSERT INTO `tb_resource` VALUES ('19', '5', '添加', 'resource:add', '1', '/resource/add.html', null, null, '0', '添加', '2016-02-28 00:01:15', '0000-00-00 00:00:00');
 INSERT INTO `tb_resource` VALUES ('20', '5', '编辑', 'resource:edit', '1', '/resource/edit.html', null, null, '0', '编辑', '2016-02-28 00:02:01', '0000-00-00 00:00:00');
 INSERT INTO `tb_resource` VALUES ('21', '5', '删除', 'resource:deleteBatch', '1', '/resource/deleteBatch.html', null, null, '0', '删除', '2016-02-28 00:03:03', '0000-00-00 00:00:00');
+INSERT INTO `tb_resource` VALUES ('22', '9', '用户操作信息', 'loginfo:userOperation', '0', '/logInfo/listUI.html', null, '', '0', '用户操作信息', '2016-03-08 22:00:36', '2016-03-08 22:02:37');
 
 -- ----------------------------
 -- Table structure for tb_resources_role
@@ -87,42 +117,44 @@ CREATE TABLE `tb_resources_role` (
   KEY `FK_r_role_resource` (`r_id`),
   CONSTRAINT `FK_r_resource_role` FOREIGN KEY (`s_id`) REFERENCES `tb_resource` (`s_id`),
   CONSTRAINT `FK_r_role_resource` FOREIGN KEY (`r_id`) REFERENCES `tb_role` (`r_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=174 DEFAULT CHARSET=utf8 COMMENT='角色权限映射表';
+) ENGINE=InnoDB AUTO_INCREMENT=229 DEFAULT CHARSET=utf8 COMMENT='角色权限映射表';
 
 -- ----------------------------
 -- Records of tb_resources_role
 -- ----------------------------
-INSERT INTO `tb_resources_role` VALUES ('133', '3', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('134', '2', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('135', '6', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('136', '9', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('137', '4', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('138', '5', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('139', '1', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('140', '11', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('141', '12', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('142', '13', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('143', '7', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('144', '8', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('145', '10', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('146', '14', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('147', '15', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('148', '16', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('149', '17', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('150', '18', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('151', '19', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('152', '20', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('153', '21', '1', '2016-02-28 00:10:17');
-INSERT INTO `tb_resources_role` VALUES ('164', '9', '2', '2016-02-28 17:11:28');
-INSERT INTO `tb_resources_role` VALUES ('165', '2', '2', '2016-02-28 17:11:28');
-INSERT INTO `tb_resources_role` VALUES ('166', '3', '2', '2016-02-28 17:11:28');
-INSERT INTO `tb_resources_role` VALUES ('167', '1', '2', '2016-02-28 17:11:28');
-INSERT INTO `tb_resources_role` VALUES ('168', '10', '2', '2016-02-28 17:11:28');
-INSERT INTO `tb_resources_role` VALUES ('169', '11', '2', '2016-02-28 17:11:28');
-INSERT INTO `tb_resources_role` VALUES ('170', '12', '2', '2016-02-28 17:11:28');
-INSERT INTO `tb_resources_role` VALUES ('171', '13', '2', '2016-02-28 17:11:28');
-INSERT INTO `tb_resources_role` VALUES ('172', '14', '2', '2016-02-28 17:11:28');
 INSERT INTO `tb_resources_role` VALUES ('173', '1', '3', '2016-02-28 17:11:47');
+INSERT INTO `tb_resources_role` VALUES ('174', '3', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('175', '2', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('176', '4', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('177', '5', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('178', '6', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('179', '9', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('180', '1', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('181', '11', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('182', '12', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('183', '13', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('184', '14', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('185', '15', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('186', '16', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('187', '17', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('188', '18', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('189', '19', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('190', '20', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('191', '21', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('192', '7', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('193', '8', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('194', '10', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('195', '22', '1', '2016-03-08 22:05:34');
+INSERT INTO `tb_resources_role` VALUES ('219', '3', '2', '2016-03-08 22:55:56');
+INSERT INTO `tb_resources_role` VALUES ('220', '2', '2', '2016-03-08 22:55:56');
+INSERT INTO `tb_resources_role` VALUES ('221', '9', '2', '2016-03-08 22:55:56');
+INSERT INTO `tb_resources_role` VALUES ('222', '1', '2', '2016-03-08 22:55:56');
+INSERT INTO `tb_resources_role` VALUES ('223', '11', '2', '2016-03-08 22:55:56');
+INSERT INTO `tb_resources_role` VALUES ('224', '12', '2', '2016-03-08 22:55:56');
+INSERT INTO `tb_resources_role` VALUES ('225', '13', '2', '2016-03-08 22:55:56');
+INSERT INTO `tb_resources_role` VALUES ('226', '14', '2', '2016-03-08 22:55:56');
+INSERT INTO `tb_resources_role` VALUES ('227', '10', '2', '2016-03-08 22:55:56');
+INSERT INTO `tb_resources_role` VALUES ('228', '22', '2', '2016-03-08 22:55:56');
 
 -- ----------------------------
 -- Table structure for tb_role
@@ -142,9 +174,9 @@ CREATE TABLE `tb_role` (
 -- ----------------------------
 -- Records of tb_role
 -- ----------------------------
-INSERT INTO `tb_user` VALUES ('4', 'admin', 'admin@webside.com', 'VaNoSnm8zZFL6eIckR9nkQ==', '0', '0', '超级管理员', '6b8c008bd7d242f1b60d713f8e3b7422', 'admin', '2016-02-15 17:17:26', '2016-03-20 16:16:46');
-INSERT INTO `tb_user` VALUES ('9', '高刚', '254813870@qq.com', 'UcsN9eTNVUpNuWqA4FZ9Ng==', '0', '0', ' 管理员', '563f0ff44bbd15b70daec5355701f363', 'admin', '2016-02-28 17:39:31', '2016-03-20 16:16:49');
-INSERT INTO `tb_user` VALUES ('10', '普通用户', '381543724@qq.com', '3kC8GTcGYkARWJG5du22+A==', '0', '0', '普通用户', '1ec5ba267bbefb7c405c95b20e815738', '高刚', '2016-02-28 20:10:05', '2016-03-20 16:17:03');
+INSERT INTO `tb_role` VALUES ('1', '超级管理员', 'administrator', '0', '超级管理员', '2016-01-05 12:07:42', '2016-02-27 22:30:15');
+INSERT INTO `tb_role` VALUES ('2', '管理员', 'admin', '0', '管理员', '2016-01-05 12:07:42', '2016-02-27 22:30:22');
+INSERT INTO `tb_role` VALUES ('3', '普通用户', 'customer', '0', '普通用户', '2016-02-28 17:09:40', '2016-03-08 22:55:36');
 
 -- ----------------------------
 -- Table structure for tb_role_user
@@ -192,9 +224,9 @@ CREATE TABLE `tb_user` (
 -- ----------------------------
 -- Records of tb_user
 -- ----------------------------
-INSERT INTO `tb_user` VALUES ('4', '高刚', 'wjggwm@126.com', '0tWdIYohJmQrWlF4VsO9uA==', '0', '0', null, 'b7c1b2ecb3c06a2e766afb516917f10e', 'wjggwm', '2016-02-15 17:17:26', '2016-02-26 17:26:29');
-INSERT INTO `tb_user` VALUES ('9', '高刚', '254813870@qq.com', 'UcsN9eTNVUpNuWqA4FZ9Ng==', '0', '0', ' 管理员', '563f0ff44bbd15b70daec5355701f363', 'wjggwm', '2016-02-28 17:39:31', '2016-02-28 19:46:04');
-INSERT INTO `tb_user` VALUES ('10', '普通用户', '381543724@qq.com', '3kC8GTcGYkARWJG5du22+A==', '0', '0', '普通用户', '1ec5ba267bbefb7c405c95b20e815738', '高刚', '2016-02-28 20:10:05', '2016-02-28 20:11:45');
+INSERT INTO `tb_user` VALUES ('4', 'admin', 'admin@webside.com', 'VaNoSnm8zZFL6eIckR9nkQ==', '0', '0', '超级管理员', '6b8c008bd7d242f1b60d713f8e3b7422', 'admin', '2016-02-15 17:17:26', '2016-03-20 16:16:46');
+INSERT INTO `tb_user` VALUES ('9', '高刚', '254813870@qq.com', 'UcsN9eTNVUpNuWqA4FZ9Ng==', '0', '0', ' 管理员', '563f0ff44bbd15b70daec5355701f363', 'admin', '2016-02-28 17:39:31', '2016-03-20 16:16:49');
+INSERT INTO `tb_user` VALUES ('10', '普通用户', '381543724@qq.com', '3kC8GTcGYkARWJG5du22+A==', '0', '0', '普通用户', '1ec5ba267bbefb7c405c95b20e815738', '高刚', '2016-02-28 20:10:05', '2016-03-20 16:17:03');
 
 -- ----------------------------
 -- Table structure for tb_user_info
@@ -217,19 +249,4 @@ CREATE TABLE `tb_user_info` (
 -- ----------------------------
 INSERT INTO `tb_user_info` VALUES ('4', '1', '2016-02-10', '15161527385', 'wjggwm@126.com', '江苏省无锡市新区', '2016-02-18 16:43:28');
 INSERT INTO `tb_user_info` VALUES ('9', null, null, null, null, null, null);
-INSERT INTO `tb_user_info` VALUES ('10', '1', '2016-02-03', '15161527385', '381543724@qq.com', '江苏省无锡市新区', null);
-
-
--- ----------------------------
--- Table structure for tb_log_info
--- ----------------------------
-DROP TABLE IF EXISTS `tb_log_info`;
-CREATE TABLE `tb_log_info` (
-  `l_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `u_id` bigint(20) NOT NULL,
-  `l_account_name` varchar(100) DEFAULT NULL,
-  `l_operation` varchar(255) DEFAULT NULL COMMENT '用户所做的操作',
-  `l_content` varchar(1000) DEFAULT NULL COMMENT '日志内容',
-  `l_create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建日期',
-  PRIMARY KEY (`l_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+INSERT INTO `tb_user_info` VALUES ('10', null, null, null, null, null, null);
